5fb8dfc040b0110e030e57c312c00d324ca455fd
Pascal Le Merrer <pascal.lemerrer@gmail.com>
2016-08-20 00:11:32 +0200

    Support Android native back button

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator } from 'react-native';
+import { AppRegistry, BackAndroid, Navigator } from 'react-native';
 
 import AlbumList from './albumlist';
 import TrackList from './tracklist';
@@ -7,11 +7,24 @@ import Search from './search';
 
 class AwesomeNativeBase extends Component {
 
+  constructor(props) {
+    super(props);
+    this.navigator = null;
+  }
 
+  componentDidMount() {
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      if (this.navigator.getCurrentRoutes().length > 1) {
+        this.navigator.pop();
+        return true;
+      }
       return false;
     });
   }
+
   renderScene(route, navigator) {
+    this.navigator = navigator;
+
     var scenes = [
       <AlbumList title={ route.title } artistId={ route.artistId } navigator={ navigator } />,
       <TrackList title={ route.title } albumId={ route.albumId } navigator={ navigator } />,
@@ -23,9 +36,10 @@ class AwesomeNativeBase extends Component {
   render() {
     return (<Navigator
         initialRoute={ { scene: 0 } }
-        renderScene={ this.renderScene }
+        renderScene={ this.renderScene.bind(this) }
       />);
   }
 }
 
 AppRegistry.registerComponent('AwesomeNativeBase', () => AwesomeNativeBase);
+
