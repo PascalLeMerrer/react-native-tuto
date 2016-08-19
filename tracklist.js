99749d24b3c100882a987b357f7461407404b1fb
Pascal Le Merrer <pascal.lemerrer@gmail.com>
2016-08-20 00:05:23 +0200

    Remove debug trace

--- tracklist.js
+++ tracklist.js
@@ -7,7 +7,6 @@ export default class TrackList extends Component {
 
     constructor(props) {
       super(props);
-      console.log('track list created')
       this.state = {
           loading: true,
           tracks: null
